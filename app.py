6981e724edda7c5665bbc72104ebbf7adb63cbac
faphys-gif <faphys@gmail.com>
2026-02-02 10:24:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ font_prop = fm.FontProperties(fname=font_path)
 # 폰트 이름 등록
 font_name = fm.FontProperties(fname=font_path).get_name()
 
-plt.rc('font', family=font_name)
+plt.rc('font', family='NanumGothic')
 # ✅ ③ 음수(-) 기호 깨짐 방지
 plt.rcParams['axes.unicode_minus'] = False
 

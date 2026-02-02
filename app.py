76d843e8302239f2af4f185a5baf157c045a838d
faphys-gif <faphys@gmail.com>
2026-02-02 10:31:22 +0900

    Clean up imports and remove cache deletion
    Removed unused imports and cache directory deletion.

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import numpy as np
 import time
-import matplotlib as mpl
-import shutil
-
-# 캐시 디렉토리 삭제
-shutil.rmtree(mpl.get_cachedir())
 
 font_path = "/usr/share/fonts/truetype/nanum/NanumGothic.ttf"  # 맑은 고딕
 font_prop = fm.FontProperties(fname=font_path)

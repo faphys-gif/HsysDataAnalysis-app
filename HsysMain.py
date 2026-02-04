dbf4f229d9c093d76f2de02ee6480f65b5d0d44c
Sang-hun Ha <faphys@gmail.com>
2026-02-04 12:16:38 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- HsysMain.py
+++ HsysMain.py
@@ -33,12 +33,12 @@ GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 biz_id = 31
 
 st.set_page_config(page_title="AI Chatbot", layout="wide")
-font_path = "./Fonts/MALGUN.TTF"
+font_path = "./Fonts/NanumGothic-Regular.ttf"
 
 # 폰트 이름 등록
 font_name = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font_name)
-plt.rc('font', family='MalgunGothic')
+plt.rc('font', family='NanumGothic')
 
 # ✅ ③ 음수(-) 기호 깨짐 방지
 plt.rcParams['axes.unicode_minus'] = False
@@ -799,6 +799,7 @@ if __name__ == "__main__":
     
 
 #https://wikidocs.net/book/14285
+
 
 
 
